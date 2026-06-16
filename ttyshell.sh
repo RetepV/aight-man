39473ae9e2d687941ec5816652d76af7a32b0cab
Peter de Vroomen <RetepV@users.noreply.github.com>
2026-06-16 17:37:54 +0200

    Change agetty terminal type to xterm-16color

--- ttyshell.sh
+++ ttyshell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo killall agetty 2> /dev/null
 AGETTY_PORT="ttyUSB0"
-sudo setsid /sbin/agetty -8 -h -J ${AGETTY_PORT} 921600,460800,230400,115200,9600,2400,110 decansi
+sudo setsid /sbin/agetty -8 -h -J ${AGETTY_PORT} 921600,460800,230400,115200,9600,2400,110 xterm-16color
 echo "agetty started on /dev/${AGETTY_PORT}"
 ps -ef | grep agetty | grep -v grep
